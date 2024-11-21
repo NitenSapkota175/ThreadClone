5c9815ccb7947530eb7f838db5b9cb64b2c4490f
NitenSapkota175 <nitensapkota1705@gmail.com>
2024-11-21 15:52:15 +0530

    url and views are created

--- users/views.py
+++ users/views.py
@@ -1,1 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import CustomUser
+from .serializers import CustomUserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+    def get_permissions(self):
+
+        if self.action == "create":  # Allow anyone to register
+            permission_classes = [permissions.AllowAny]
+        else:  # Restrict other actions to authenticated users
+            permission_classes = [permissions.IsAuthenticated]
+        return [permission() for permission in permission_classes]
